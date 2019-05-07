b6b96f0d79f8214db8bb5d247f09832f98a47f2d
Eric Zheng <zheng.eric.y@gmail.com>
2019-05-07 15:49:01 -0400

    init repository

---
+++ README.md
@@ -0,0 +1,10 @@
+# mv-model
+
+This repository contains modeling work done for Dr. Eng's multivariable calculus final project.
+
+## Dependencies
+
+* Python 3
+* Numpy/Matplotlib
+* NetworkX
+
---
+++ build-network.py
@@ -0,0 +1,17 @@
+# build a networked model from node data in some CSV(s)
+
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+
+class Node(object):
+    pass
+
+class Sink(Node):
+    pass
+
+class Source(Node):
+    pass
+
+print('Hello')
+
